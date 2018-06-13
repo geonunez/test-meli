f0c78e8282c2a939e983fd411207eed07ad5dade
Jorge Nuñez <jnunez@toolboxtve.com>
2018-06-13 16:44:50 -0300

    Found transpose and one diagonal

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 
 ## Nota
 Este proyecto es para aplicar a una vacante de trabajo. Algunos aspectos son tomadas por sentado.
+
+'ATGCGA'
+'CAGTGC'
+'TTATGT'
+'AGAAGG'
+'CCCCTA'
+'TCACTG'
+
+['A' 'T' 'G' 'C' 'G' 'A']
+['C' 'A' 'G' 'T' 'G' 'C']
+['T' 'T' 'A' 'T' 'G' 'T']
+['A' 'G' 'A' 'A' 'G' 'G']
+['C' 'C' 'C' 'C' 'T' 'A']
+['T' 'C' 'A' 'C' 'T' 'G']
+
+['A' 'C' 'T' 'A' 'C' 'T']
+['T' 'A' 'T' 'G' 'C' 'C']
+['G' 'G' 'A' 'A' 'C' 'A']
+['C' 'T' 'T' 'A' 'C' 'C']
+['G' 'G' 'G' 'G' 'T' 'T']
+['A' 'C' 'T' 'G' 'A' 'G']
+
+['T']
+['C', 'C']
+['A', 'C', 'A']
+['T', 'G', 'C', 'C']
+['C', 'T', 'A', 'C', 'T']
+['A', 'A', 'A', 'A', 'T', 'G']
+['T', 'G', 'T', 'G', 'A']
+['G', 'T', 'G', 'G']
+['C', 'G', 'T']
+['G', 'C']
+['A']
--- api/services.py
+++ api/services.py
@@ -1,3 +1,4 @@
+import numpy as np
 class MutantService:
     """
     Mutant Services
@@ -5,10 +6,11 @@ class MutantService:
 
     @staticmethod
     def is_mutant(adn):
-        is_mutant = False
         adn = MutantService.convert_to_bidimensional(adn)
 
-        return is_mutant
+        return (MutantService.verify_horizontal(adn) or
+            MutantService.verify_vertical(adn) or
+            MutantService.verify_diagonal(adn))
 
     @staticmethod
     def convert_to_bidimensional(adn):
@@ -17,3 +19,28 @@ class MutantService:
             bidimensional.append(list(sequence))
 
         return bidimensional
+
+    @staticmethod
+    def verify_horizontal(adn):
+        print(adn)
+        return False
+
+    @staticmethod
+    def verify_vertical(adn):
+        adn = np.transpose(adn)
+        print(adn)
+        return False
+
+    @staticmethod
+    def verify_diagonal(adn):
+        x = len(adn[0])
+        y = len(adn) -1
+        diags = []
+        for i in range(y * -1, 0):
+            diags.append(np.diagonal(adn, i, -1, 0))
+        for i in range(0, x):
+            diags.append(np.diagonal(adn, i, -1, 0))
+
+        print(diags)
+
+        return False
