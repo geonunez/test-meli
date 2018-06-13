7df9a17d02556433ee4b200547b9cb34cdb132d3
Jorge Nuñez <jorge.nunez@geonunez.com>
2018-06-12 22:54:04 -0300

    Created MutantSerializer

---
+++ api/services.py
@@ -0,0 +1,9 @@
+class MutantService:
+    """
+    Mutant Services
+    """
+
+    @staticmethod
+    def is_mutant(adn):
+        print(adn);
+        return True
--- api/views.py
+++ api/views.py
@@ -6,7 +6,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+from .services import MutantService
+from .serializers import MutantSerializer
+
 class MutantViewSet(ViewSet):
 
     def verify(self, request):
-        return Response(status=status.HTTP_200_OK)
+        serializer = MutantSerializer(request.data)
+        data = serializer.data;
+        print(data)
+
+        if MutantService.is_mutant(data.adn):
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
