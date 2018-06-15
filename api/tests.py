f6b3eeccfd03a824f5140f9c798eff7b7dced9a8
Jorge Nuñez <jnunez@toolboxtve.com>
2018-06-15 15:33:25 -0300

    Improved test

--- api/tests.py
+++ api/tests.py
@@ -1,33 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from django.test import TestCase
+import time
 
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-class HumanTestCase(TestCase):
+from .models import Human
 
+class HumanTestCase(TestCase):
+    client = APIClient()
 
     def test_invalid_parameters(self):
         dnas = [
             # Not has then minimun len
-            [
-                'AAA',
-                'CCC',
-                'TTT'
-            ],
+            ['AAA', 'CCC', 'TTT'],
             # Not is a NxN
-            [
-                'AAAA',
-                'CCCC',
-                'TTTT'
-            ],
-            [
-                'AAA',
-                'CCC',
-                'TTT',
-                'GGG'
-            ],
+            ['AAAA', 'CCCC', 'TTTT'],
+            ['AAA', 'CCC', 'TTT', 'GGG'],
             [
                 'ACACACCACACACACACACACACAC',
                 'TGTGATGAAATGGGGAAAAAAATGG',
@@ -45,81 +35,117 @@ class HumanTestCase(TestCase):
                 'TGTGATGAAATGGGGAAAAAAATGG',
             ],
             # Some column breaks the matrix len
-            [
-                'AAAA',
-                'TTT',
-                'GGGG',
-                'CCCC'
-            ]
+            ['AAAA', 'TTT', 'GGGG', 'CCCC']
         ]
 
         for dna in dnas:
-            client = APIClient()
-            response = client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+            s_dna = ''.join(dna)
+            response = self.client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+            how_many = Human.objects.filter(dna=s_dna).count()
+
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+            self.assertEqual(how_many, 0)
 
     def test_mutant(self):
         dnas = [
-            # two or more in horizontal
-            [
-                'AAAA',
-                'CCGC',
-                'TTTT',
-                'AGAG'
-            ],
-            [
-                'ACGA',
-                'TTTT',
-                'CCCC',
-                'AAAA'
-            ],
-            [
-                'TTTT',
-                'GGGG',
-                'AAAA',
-                'CCCC'
-            ],
-            [
-                'TATTCA',
-                'TTAAAA',
-                'GGGGTA',
-                'TCAGCA',
-                'CTAGAG',
-                'TCCCAG'
-            ],
-            # two or more in vertical
-            [
-                'AATA',
-                'ACTC',
-                'ATTT',
-                'AGTG'
-            ],
-            [
-                'TGGT',
-                'TGGT',
-                'AGGA',
-                'CGGC'
-            ],
-            [
-                'ACAA',
-                'ACAA',
-                'AGAA',
-                'ATAA'
-            ],
-            # two or more in diagonal
-            [
-                'CCGTTG',
-                'CAGGAG',
-                'TGACGT',
-                'GGGACG',
-                'TTTGAT',
-                'TGAAGC'
-            ],
+            # two or more in →
+            ['AAAA', 'CCGC', 'TTTT', 'AGAG'],
+            ['ACGA', 'TTTT', 'CCCC', 'AAAA'],
+            ['TTTT', 'GGGG', 'AAAA', 'CCCC'],
+            ['TATTCA', 'TTAAAA', 'GGGGTA', 'TCAGCA', 'CTAGAG', 'TCCCAG'],
+            # two or more in ↓
+            ['AATA', 'ACTC', 'ATTT', 'AGTG'],
+            ['TGGT', 'TGGT', 'AGGA', 'CGGC'],
+            ['ACAA', 'ACAA', 'AGAA', 'ATAA'],
+            # two or more in ↘
+            ['CCGTTG','CAGGAG','TGACGT','GGGACG','TTTGAT','TGAAGC'],
+            # two or more in ↗
+            ['CCGTTG', 'CAGTAG', 'TGTCGT', 'GTGGCG', 'TTGGTT', 'TGAAGC'],
+            # two or more in →↗↘↓
+            ['CCGTTG', 'CAGTAG', 'TGGCGT', 'GGGGCG', 'TTGGTT', 'TGGAGC'],
+            # Repeated ones
+            ['AAAA', 'CCGC', 'TTTT', 'AGAG'],
+            ['AATA', 'ACTC', 'ATTT', 'AGTG'],
+            ['CCGTTG','CAGGAG','TGACGT','GGGACG','TTTGAT','TGAAGC'],
+            ['CCGTTG', 'CAGTAG', 'TGTCGT', 'GTGGCG', 'TTGGTT', 'TGAAGC'],
+            ['CCGTTG', 'CAGTAG', 'TGGCGT', 'GGGGCG', 'TTGGTT', 'TGGAGC'],
         ]
 
         for dna in dnas:
-            client = APIClient()
-            response = client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+            s_dna = ''.join(dna)
+            response = self.client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+            how_many = Human.objects.filter(dna=s_dna).count()
+
             self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(how_many, 1)
+
+    def test_humans(self):
+        dnas = [
+            # Zero appears
+            ['ATAA', 'CCGC', 'TATT', 'AGAG'],
+            ['ACGA', 'TCTT', 'CTCC', 'AAAA'],
+            ['TATTCA', 'TTATAA', 'GTGGTA', 'TCAGCA', 'CTAGAG', 'TCCCAG'],
+            # One appear
+            ['AAAA', 'CCGC', 'TATT', 'AGAG'], # →
+            ['ACGA', 'TCTT', 'TCCC', 'ACAA'], # ↓
+            ['ACGA', 'TATT', 'TCAC', 'ACAA'], # ↘
+            ['ACGA', 'TCAT', 'TACC', 'ACAA'], # ↗
+            # Repeated ones
+            ['ATAA', 'CCGC', 'TATT', 'AGAG'],
+            ['AAAA', 'CCGC', 'TATT', 'AGAG'], # →
+            ['ACGA', 'TCTT', 'TCCC', 'ACAA'], # ↓
+            ['ACGA', 'TATT', 'TCAC', 'ACAA'], # ↘
+            ['ACGA', 'TCAT', 'TACC', 'ACAA'], # ↗
+        ]
+
+        for dna in dnas:
+            s_dna = ''.join(dna)
+            response = self.client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+            how_many = Human.objects.filter(dna=s_dna).count()
+
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+            self.assertEqual(how_many, 1)
+
+    def test_stats(self):
+        # Check with an empty db
+        data = self.client.get('/api/v1/stats').data
+
+        self.assertEqual(data['count_mutant_dna'], 0)
+        self.assertEqual(data['count_human_dna'], 0)
+        self.assertEqual(data['radio'], 0.0)
+
+        # Populate the db
+        dnas = [
+            # Humans
+            ['ATAA', 'CCGC', 'TATT', 'AGAG'],
+            ['TATTCA', 'TTATAA', 'GTGGTA', 'TCAGCA', 'CTAGAG', 'TCCCAG'],
+            ['AAAA', 'CCGC', 'TATT', 'AGAG'], # →
+            ['ACGA', 'TCTT', 'TCCC', 'ACAA'], # ↓
+            ['ACGA', 'TATT', 'TCAC', 'ACAA'], # ↘
+            ['ACGA', 'TCAT', 'TACC', 'ACAA'], # ↗
+            # Mutans
+            ['AAAA', 'CCGC', 'TTTT', 'AGAG'],
+            ['AATA', 'ACTC', 'ATTT', 'AGTG'],
+            ['CCGTTG','CAGGAG','TGACGT','GGGACG','TTTGAT','TGAAGC'],
+            ['CCGTTG', 'CAGTAG', 'TGTCGT', 'GTGGCG', 'TTGGTT', 'TGAAGC'],
+        ]
+
+        for dna in dnas:
+            self.client.post('/api/v1/mutant', { 'dna': dna }, format='json')
+
+        # Check if cache it's working
+        data = self.client.get('/api/v1/stats').data
+
+        self.assertEqual(data['count_mutant_dna'], 0)
+        self.assertEqual(data['count_human_dna'], 0)
+        self.assertEqual(data['radio'], 0.0)
+
+        # Wait until the cache expires
+        time.sleep(10)
+        data = self.client.get('/api/v1/stats').data
+
+        self.assertEqual(data['count_mutant_dna'], 4)
+        self.assertEqual(data['count_human_dna'], 6)
+        self.assertEqual(data['radio'], 0.4)
 
 
